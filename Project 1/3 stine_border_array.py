21f88335ee19b45051dec6bc64808292544bc317
stinekrye <201706401@post.au.dk>
2021-09-14 11:07:17 +0200

    borderarray working, but in O(n+m)

--- Project 1/3 stine_border_array.py
+++ Project 1/3 stine_border_array.py
@@ -1,14 +1,12 @@
-x = "abcab"
+x = "abcabc"
 
 
 def borderarray(x):
     ba = list([0] * len(x))
     for i in range(1,len(x)):
         b = ba[i-1]
-        j = i
-        while x[b] != x[j] and len(ba[0:j]) > 0: # I'm not sure that we get to the last index and extend the empty string
-            j -= 1
-            b = ba[j-1]
+        while x[b] != x[i] and b > 0: # I'm not sure that we get to the last index and extend the empty string
+            b -= 1
         if x[b] == x[i]:
             ba[i] = b + 1
 
