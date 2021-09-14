1deac4b29eb679976e8f0ed3cbe2318168997156
stinekrye <201706401@post.au.dk>
2021-09-14 11:43:32 +0200

    -

--- Project 1/3 stine_border_array.py
+++ Project 1/3 stine_border_array.py
@@ -1,4 +1,5 @@
-x = "abcabc"
+x = "abca"
+m = "abcannabcnnabc"
 
 
 def borderarray(x):
@@ -12,4 +13,21 @@ def borderarray(x):
 
     return ba
 
-print(borderarray(x))
+def bordersearch(x,m):
+    ba = borderarray(x)
+    print(ba)
+    bm = list([0] * len(m))
+    for i in range(len(m)):
+        b = ba[len(x)-1]
+        while x[b] != m[i] and b > 0: # I'm not sure that we get to the last index and extend the empty string
+            b -= 1
+        if x[b] == m[i]:
+            bm[i] = bm[i-1] + 1
+
+    return bm
+
+
+# It does not catch the second match
+
+
+print(bordersearch(x,m))
