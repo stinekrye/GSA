23ae58b3b4ae8b27da06cfd9a8fc89e2c8936c37
stinekrye <201706401@post.au.dk>
2021-09-19 20:51:38 +0200

    -

--- Project 1/test_naive.py
+++ Project 1/test_naive.py
@@ -1,13 +1,32 @@
-from search_naive import naive_matching
-from read_fasta import read_fasta_file
-from read_fastq import read_fastq_file
+from search_naive_stine import naive_matching
+import random
 
-# x er fasta file, p er fastq file
+#%%
 
-x = read_fasta_file("stinetest.fasta")
-p = read_fastq_file("fastqtest.fastq")
+def dna(length=int(), letters="CGTA"):
+    return ["seq",''.join(random.choices(letters, k=length))]
 
-res = naive_matching()
-print(p["Seq1"][0])
-print(x)
-# naive_matching(x,p)
+def fastq(dna): # Third row with + is omitted, because it is excluded by fastq reader
+    res = []
+    first = '@Seq'
+    second = "{}".format(dna[1])
+    fourth = "{}".format('~' * len(dna[1]))
+    res.append(first)
+    res.append([second,fourth])
+    res.append(fourth)
+    return res
+
+
+#%%
+
+fasta = dna(20)
+idx = random.randrange(5,len(fasta[1]),5)
+seq1 = fasta[:5]
+seq2 = fasta[-5:]
+seq3 = fasta[idx:idx+6]
+
+p = fastq(seq1)
+# print(p[1][1])
+
+res = naive_matching(fasta,p)
+print(res)
