9a2f514a5be7d3204e046384913cfaebc7fbb9be
stinekrye <201706401@post.au.dk>
2021-09-20 15:36:22 +0200

    Test scripts are made

---
+++ Project 1/test_scripts/test_search_ba_stine.py
@@ -0,0 +1,65 @@
+from unittest import TestCase
+from search_ba_stine import ba_search
+import random
+random.seed(367)
+# Fails when has to deal with two or more matches. Problems with the way I have defined the result. This will be easier, when I can use this alg as "expected result"
+
+#%%  Helper functions
+def dna(length=int(), letters="CGTA"):
+    return ["seq", ''.join(random.choices(letters, k=length))]
+
+
+def fastq(dna):  # Third row with + is omitted, because it is excluded by fastq reader
+    res = []
+    first = '@Seq'
+    second = "{}".format(dna)
+    fourth = "{}".format('~' * len(dna))
+    res.append(first)
+    res.append([second, fourth])
+    res.append(fourth)
+    return res
+
+#%% Test 1 data BEGINNING                              # make this into a table to test 5 times each case
+x1 = dna(20)
+seq1 = x1[1][:5]
+p1 = fastq(seq1)
+real_res1 = "@Seq 0 seq {} 0 {}M * 0 0 {} {}".format(1, len(seq1), seq1, "~" * len(seq1))
+
+#%% Test 2 data END
+x2 = dna(20)
+seq2 = x2[1][-5:]
+p2 = fastq(seq2)
+real_res2 = "@Seq 0 seq {} 0 {}M * 0 0 {} {}".format(len(x2[1])-5+1, len(seq2), seq2, "~" * len(seq2))
+
+#%% Test 3 data RANDOM
+x3 = dna(20)
+idx = random.randrange(5,len(x3[1]),5)
+seq3 = x3[1][idx:idx+6]
+p3 = fastq(seq3)
+real_res3 = "@Seq 0 seq {} 0 {}M * 0 0 {} {}".format(idx+1, len(seq3), seq3, "~" * len(seq3))
+
+
+#%% Test 4 data fasta is empty
+x4 = ""
+p4 = fastq(seq3)
+
+#%% Test 5 data fasta is empty
+x5 = dna(20)
+p5 = ""
+
+class Test(TestCase):
+    def testBeginning(self):
+        test_res1 = ba_search(x1, p1)
+        self.assertEqual(test_res1, real_res1)
+    def testEnd(self):
+        test_res2 = ba_search(x2, p2)
+        self.assertEqual(test_res2, real_res2)
+    def testRandom(self):
+        test_res3 = ba_search(x3, p3)
+        self.assertEqual(test_res3, real_res3)
+    def testEmptyFasta(self):
+        with self.assertRaises(IndexError):
+            ba_search(x4, p4)
+    def testEmptyFastq(self):
+        with self.assertRaises(IndexError):
+            ba_search(x5, p5)
