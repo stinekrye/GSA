2937d01fb9bc05882d3b4874a571cf243ebd2f89
janneengestoft <jengestoft@gmail.com>
2021-09-21 10:04:59 +0200

    Updated algorithms to handle empty input files

--- Project 1/3 search_ba.py
+++ Project 1/3 search_ba.py
@@ -35,32 +35,37 @@ def borderarray(x):
 
 # Search function
 def search_ba(x, p):
-    # Definitions for the SAM format
-    qname = p[0]
-    flag = 0
-    rname = x[0]
-    pos = 0
-    mapq = 0
-    substring = p[1][0]
-    cigar = str(len(substring)) + 'M'
-    rnext = '*'
-    pnext = 0
-    tlen = 0
-    qual = p[1][1]
 
-    # Other definitions
-    rseq = x[1]
-    m = len(substring)
-    s = substring + '$' + rseq
-    b = borderarray(s)
+    if len(x) > 0 and len(p) > 0:
+        # Definitions for the SAM format
+        qname = p[0]
+        flag = 0
+        rname = x[0]
+        pos = 0
+        mapq = 0
+        substring = p[1][0]
+        cigar = str(len(substring)) + 'M'
+        rnext = '*'
+        pnext = 0
+        tlen = 0
+        qual = p[1][1]
 
-    for i in range(len(b)):
-        if b[i] == m:
-            pos = i - 2 * m + 1
-            print(
-                qname, flag, rname, pos, mapq, cigar, rnext, pnext, tlen, substring, qual, 
-                file = sys.stdout)
-            pos = 0
+        # Other definitions
+        rseq = x[1]
+        m = len(substring)
+        s = substring + '$' + rseq
+        b = borderarray(s)
+
+        for i in range(len(b)):
+            if b[i] == m:
+                pos = i - 2 * m + 1
+                print(
+                    qname, flag, rname, pos, mapq, cigar, rnext, pnext, tlen, substring, qual, 
+                    file = sys.stdout)
+                pos = 0
+    
+    else:
+        print('One or both input files are empty')
 
 for item in fastq.items():
     for seq in fasta.items():
