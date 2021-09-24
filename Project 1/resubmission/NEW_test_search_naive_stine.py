1676cfe35647b23057859e27e6e55003c408abe5
stinekrye <201706401@post.au.dk>
2021-09-24 11:39:17 +0200

    Working on resubmission

--- Project 1/resubmission/NEW_test_search_naive_stine.py
+++ Project 1/resubmission/NEW_test_search_naive_stine.py
@@ -9,13 +9,21 @@ random.seed(367)
 #%% Test 1 data compare output of testfile                           # make this into a table to test 5 times each case
 fasta = read_fasta_file(f"../fasta_test.fasta")
 fastq = read_fastq_file(f"../fastq_test.fastq")
+# fasta1 = read_fasta_file(f"../fasta_test1.fasta")
+# fastq1 = read_fastq_file(f"../fastq_test1.fastq")
 
 
 
 class TestRandom(TestCase):
-    def testBeginning(self):
+    def testMissisipi(self):
         naive_matching(fasta,fastq)
         test = sys.stdout.getvalue().strip()
         search_ba_wrapper(fasta,fastq)
         right_answer = sys.stdout.getvalue().strip()
         self.assertEqual(test, right_answer)
+    # def testOverlapping(self):
+    #     naive_matching(fasta1,fastq1)
+    #     test = sys.stdout.getvalue().strip()
+    #     search_ba_wrapper(fasta1,fastq1)
+    #     right_answer = sys.stdout.getvalue().strip()
+    #     self.assertEqual(test, right_answer)
