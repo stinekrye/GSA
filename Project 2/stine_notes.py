9b93fadea63134a7f3a26d5e6316af984a865b1f
stinekrye <201706401@post.au.dk>
2021-10-12 14:25:35 +0200

    Search is almost working. Need to adjust self.seq[self.root.range_start + x] so it starts at the current node

--- Project 2/stine_notes.py
+++ Project 2/stine_notes.py
@@ -6,6 +6,17 @@ def remap(x):
     n = [m[a] for a in x]
     return n, m
 
+def return_labels(node):
+    if node.string_label != None:
+        a = node.string_label
+        return a
+    else:
+        for child in node.children:
+            if child != None:
+                if child.string_label != None:
+                    return child.string_label
+                else:
+                    return_labels(child)
 
 class Node():
     def __init__(self, range_start = None, range_end = None, string_label = None, children = [None,None,None,None,None], parent = None):
@@ -23,7 +34,7 @@ class SuffixTree():
 
     def __init__(self, seq = None):
         self.seq = seq
-        self.root = Node()
+        self.root = Node(range_start = 0, range_end = 0)
         self.n = 0
  #       self.root.parent = self.root
 
@@ -39,7 +50,7 @@ class SuffixTree():
         return
 
 
-    def split_edge(self,c,x):
+    def split_edge(self,c,x,i,n):
         c_char = self.seq[c.range_start]                                                # first character in c
         u = Node(range_start = c.range_start, range_end = c.range_start + x, parent = c.parent, children=[None, None, None, None, None])    # create u node
         c.parent.children[c_char] = u                                                   # update the child of P(c) to be u. The first character in u is c_char
@@ -58,7 +69,7 @@ class SuffixTree():
             if x + i == child.range_end:  # if we reach the end of the edge, we have to find the new outgoing edge to search from
                 self.naive_insert(child, x, i, n) # Be carefull with this recursion
             else:
-                u = self.split_edge(child, x)
+                u = self.split_edge(child, x, i, n)
                 self.insert_child(u, x, i, n)
 
 
@@ -84,28 +95,31 @@ class SuffixTree():
         while i < len(y):                                                                    # Change this to iterate through the full sequence when done
             self.naive_insert(self.root,x,i,n)                                                  # input = suffix tree, root to search from, x (incrementor), i = string label. n+1 = length of sequence
             i += 1
-    def find_edge_search(self,c, char):
+
+
+    def find_edge_search(self, c, char):
         return c.children[char]
 
-    def search_rec(self,child,x,n):
+    def search_rec(self,c,x,n):
+        child = self.find_edge_search(c, n[x])
         if child:  # If we have an outgoing edge
-            while x < child.range_end and self.seq[child.range_start + x] == n[x]:  # If we have have not reached the end of the edge yet and we can extend the match
+            while x < child.range_end and x < len(n) and self.seq[self.root.range_start + x] == n[x]:  # self.root.range_start has to be the beginning of the match. In other words: We have to begin at each node instead of the root
                 x += 1
-            if x == child.range_end and x < len(n)-1:  # if we reach the end of the edge, and we have not found a match, we have to search down a new node
-                search_rec(child,x,n)
-            elif x == child.range_end and x == len(n)-1:
-                return "Match found"  # Be carefull with this recursion
-            # else:
-            #     return "Match not found in internal node"
+            if x == child.range_end and x < len(n):  # if we reach the end of the edge, and we have not found a match, we have to search down a new node
+                return self.search_rec(child,x,n)
+            elif x == len(n):  # if we reach the end of an edge and we find a match (also works on edges
+                res = return_labels(child)
+                return res  # Be carefull with this recursion
+            else:
+                return "Match not found in branch"
         else:
-            return "Match not found in root"
+            return "Match not found in node"
 
     def search(self,y):
         n = [self.alpha[a] for a in y]
         c = self.root
         x = 0
-        child = find_edge_search(c, n[x])
-        res = search_rec(child,x,n)
+        res = self.search_rec(c,x,n)
         return res
 
 
@@ -117,5 +131,7 @@ x = "GTCGTA"
 res = SuffixTree()
 res.insert(x)
 # print(res.n)
-n = res.search("GT")
+n = res.search("GTA")
 print(n)
+# for i in n:
+#     print(i)
