9d9449e0551d7c7536009b9614811e3f5fb95561
stinekrye <201706401@post.au.dk>
2021-10-12 21:20:34 +0200

    I'm never doing this again

---
+++ Project 2/read_fasta.py
@@ -0,0 +1,19 @@
+def read_fasta_file(filename):
+    sequences_lines = {}
+    current_sequence_lines = None
+    with open(filename) as fp:
+        for line in fp:
+            line = line.strip()
+            if line.startswith(';') or not line:
+                continue
+            if line.startswith('>'):
+                sequence_name = line.lstrip('>')
+                current_sequence_lines = []
+                sequences_lines[sequence_name] = current_sequence_lines
+            else:
+                if current_sequence_lines is not None:
+                    current_sequence_lines.append(line)
+    sequences = {}
+    for name, lines in sequences_lines.items():
+        sequences[name] = ''.join(lines)
+    return sequences
