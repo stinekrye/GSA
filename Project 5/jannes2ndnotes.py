0e5e51ca510af572f2e193fdb5bb97265552a979
janneengestoft <jengestoft@gmail.com>
2021-11-28 19:05:44 +0100

    .

---
+++ Project 5/jannes2ndnotes.py
@@ -0,0 +1,209 @@
+import gen_lcp, parsers
+import sys, argparse
+
+def remap(x):
+    m = {a:i for i,a in enumerate(sorted(set(x)))}
+    n = [m[a] for a in x]
+    return n, m
+
+def return_labels(node):
+    stack = [node]
+    # stack.append(node)
+
+    while len(stack) > 0:
+        if stack[-1].string_label != None:
+            yield stack[-1].string_label
+            stack.pop(-1)
+            # p.parent = None
+            # p.children = None
+
+        else:
+            parent = stack.pop(-1)
+            for child in parent.children:
+                if child != None:
+                    stack.append(child)
+            # parent.parent = None
+            # parent.children = None
+
+class Node():
+    def __init__(self, range_start = None, range_end = None, string_label = None, children = [], parent = None):
+        self.range_start = range_start
+        self.range_end = range_end
+        self.string_label = string_label
+        self.children = children
+        self.parent = parent
+
+    def __repr__(self):
+        rep = f"Node({self.range_start},{self.range_end})"
+        return rep
+
+class SuffixTree2():
+
+    def __init__(self, seq = None):
+        self.seq = seq
+        self.root = Node(range_start = 0, range_end = 0)
+        self.n = 0
+        self.alpha = None
+
+    def append_child(self, v, sa, lcp, length):
+        new_leaf = Node(
+            range_start=sa+lcp,
+            range_end=length,
+            string_label=sa,
+            parent = v,
+            children=[]
+        )
+        v.children.append(new_leaf)
+
+        return new_leaf
+
+    def split_edge(self, v, length_up):                       
+        # New node
+        u = Node(
+            range_start = v.range_start, 
+            range_end = v.range_end - length_up, 
+            parent = v.parent, 
+            children=[],
+            string_label=None
+            )
+
+        # Adding u to the children list of v's parent and removing v from it
+        v.parent.children.append(u)
+        v.parent.children.remove(v)
+
+        # Update v  
+        v.parent = u                                                                    
+        v.range_start = v.range_end - length_up
+
+        # Add v as child of u                                                                                         
+        u.children.append(v)
+
+        return u
+    
+    def lcp_insert(self, i, sa, lcp, v, length):
+        length_up = length - sa[i-1] - lcp[i]
+        v_edge_length = v.range_end - v.range_start
+
+        while length_up >= v_edge_length and length_up != 0:
+            length_up -= v_edge_length
+            v = v.parent
+            v_edge_length = v.range_end - v.range_start
+        
+        if length_up == 0:
+            new_leaf = self.append_child(v, sa[i], lcp[i], length)
+        else: 
+            u = self.split_edge(v, length_up)
+            new_leaf = self.append_child(u, sa[i], lcp[i], length)
+
+        return new_leaf
+
+    def lcp_suffix_tree(self, sa, lcp):
+        seq_length = len(sa)
+
+        # Initialize with SA[0]
+        v = Node(
+            range_start=sa[0], 
+            range_end=seq_length, 
+            string_label=sa[0], 
+            children=[], 
+            parent=self.root)
+
+        # Make it child of root
+        self.root.children.append(v)
+
+        # Insert the rest
+        for i in range(1, seq_length):
+            v = self.lcp_insert(i, sa, lcp, v, seq_length)
+
+    def find_edge_search(self, c, char):
+        for child in c.children:
+            if self.seq[child.range_start] == char:
+                return child
+
+    def search_rec(self,c,x,k, n):
+
+        child = self.find_edge_search(c, n[x])
+        if child:  # If we have an outgoing edge
+            k = child.range_start
+            while k < child.range_end and x < len(n) and self.seq[k] == n[x]:  # self.root.range_start has to be the beginning of the match. In other words: We have to begin at each node instead of the root
+                x += 1
+                k += 1
+            if k >= child.range_end and x < len(n):  # if we reach the end of the edge, and we have not found a match, we have to search down a new node
+                return self.search_rec(child, x, k, n)
+            elif x == len(n):  # if we reach the end of an edge and we find a match (also works on edges
+                res = return_labels(child)
+                return res  # Be carefull with this recursion
+            else:
+                return None
+        else:
+            return None
+
+
+# Wrapper function
+def search_suffix(sa_lcp, fastq):
+
+    if len(sa_lcp) < 0 or len(fastq) < 0:
+        return "Problems with either fastq file or the SA and LCP"
+
+    flag, mapq, pnext, tlen = 0,0,0,0
+    rnext = "*"
+
+    for x in sa_lcp.items():
+        rname = x[0]
+        tree = None
+        tree = SuffixTree2()
+        tree.root.children = []
+        y = x[1][0] + "0"
+        tree.seq, tree.alpha = remap(y)
+        sa = x[1][1]
+        lcp = x[1][2]
+        tree.lcp_suffix_tree(sa, lcp)
+
+
+        for p in fastq.items():
+            qname = p[0]
+            substring = p[1][0]
+            cigar = str(len(substring)) + "M"
+            qual = p[1][1]
+
+            seq = [tree.alpha[a] for a in substring]
+
+            matches = tree.search_rec(tree.root, 0, 0, seq)
+
+            if matches is not None:
+                for match in matches:
+                    pos = int(match) + 1
+                    print(f"{qname}\t{flag}\t{rname}\t{pos}\t{mapq}\t{cigar}\t{rnext}\t{pnext}\t{tlen}\t{substring}\t{qual}", file = sys.stdout)
+
+sa_lcp = parsers.read_SA_LCP("SA_LCP_fasta1.txt")
+fastq = parsers.read_fastq_file("fastq_test.fq")
+search_suffix(sa_lcp, fastq)
+
+#### RUNNING THE SCRIPT
+# If the -p option is given with a fastafile (e.g. "python search_st2.py -p test.fasta"), 
+# the script will construct a suffix tree, create the SA and LCP from it, and output the 
+# two arrays to a textfile named "SA_LCP_name". Where the name is the sequence name from 
+# the fastafile.
+# If the script is not given the -p option, it takes two input files: a fastq file and a 
+# text file containing the SA and LCP. From this it constructs a suffix tree and searches
+# for the pattern found in the fastq file.
+
+# # Creating first parser
+# parser1 = argparse.ArgumentParser(description='SA and LCP computation from suffix tree')
+# parser1.add_argument('-p', help='Create SA and LCP from fastafile')
+# args1 = parser1.parse_known_args()
+
+# if args1[0].p:
+#     fastafile = parsers.read_fasta_file(args1[0].p)
+#     fastaname = f"{args1[0].p}"
+#     gen_lcp.gen_lcp(fastafile, fastaname)
+
+# else:
+#     # Creating second parser if -p is not given
+#     parser2 = argparse.ArgumentParser(description='Pattern matching using suffix tree')
+#     parser2.add_argument('fastafile', help="Input fasta file")
+#     parser2.add_argument('fastqfile', help="Input fastq file")
+#     args2 = parser2.parse_args()
+#     sa_lcp = parsers.read_SA_LCP(f"{args2.fastafile}.sa-lcp")
+#     fastq = parsers.read_fastq_file(args2.fastqfile)
+#     search_suffix(sa_lcp, fastq)
---
+++ Project 5/jannesnotes.py
@@ -0,0 +1,217 @@
+import numpy as np
+import gen_sa, parsers
+import sys, argparse
+
+def c_table(sa):
+    buckets = None
+
+    for key, value in sa.items():
+        buckets = {}
+        sa = value[1]
+        seq = value[0] + "$"
+        
+        for i in range(0, len(sa)):
+            char = seq[sa[i]]
+            if char not in buckets:
+                buckets[char] = i
+
+    return buckets
+
+def o_table(sa):
+
+    for key, value in sa.items():
+        sa = value[1]
+        seq = value[0] + "$"
+
+        alphabet = sorted(set(seq))
+        alphabet_size = len(alphabet)
+        n = len(sa)
+
+        O = np.zeros((n + 1, alphabet_size))
+        
+        for i in range(1, n + 1):
+            for a in range(0, alphabet_size):
+                if seq[sa[i - 1] - 1] == alphabet[a]:
+                    O[i][a] = O[i - 1][a] + 1
+                else: 
+                    O[i][a] = O[i - 1][a]
+
+    return O
+
+def d_table(RO, C, sa, p, alpha):
+    min_edits = 0
+    m = len(p)
+    L, R = 0, len(sa)
+    d = np.zeros(m)
+    rev_p = p[::-1]
+
+    for i in range(m):
+        a = rev_p[i]
+        L = C[a] + RO[int(L)][alpha[a]]
+        R = C[a] + RO[int(R)][alpha[a]]
+        if L >= R:
+            min_edits += 1
+            L = 0
+            R = len(sa)
+        d[i] = min_edits
+
+    return d
+
+x = "mississippi$"
+sa_dict = {"one":["mississippi", [11, 10, 7, 4, 1, 0, 9, 8, 6, 3, 5, 2]]}
+sa = [11, 10, 7, 4, 1, 0, 9, 8, 6, 3, 5, 2]
+rsa_dict = {"one":["ippississim", [11, 9, 0, 6, 3, 10, 2, 1, 8, 5, 7, 4]]}
+p = "iss"
+alpha = {a:i for i, a in enumerate(sorted(set(x)))}
+O = o_table(sa_dict)
+C = c_table(sa_dict)
+RO = o_table(rsa_dict)
+d = d_table(RO, C, sa, p, alpha)
+
+d_table(RO, C, sa, p, alpha)
+
+def bw_approx(O, C, p, d, sa, alpha, max_edits):
+    matches = None
+    L, R = 0, len(sa)
+    i = len(p) - 1
+    cigar = ''
+    no_edits = 0
+    
+    # Match
+    for a in list(alpha.keys())[1:]:
+        new_L = C[a] + O[int(L)][alpha[a]]
+        new_R = C[a] + O[int(R)][alpha[a]]
+        
+        if a == p[i]:
+            edit_cost = 0
+        else:
+            edit_cost = 1
+        
+        if max_edits - edit_cost < 0: continue
+        if new_L >= new_R: continue
+        cigar += 'M'
+        matches = rec_approx(
+            d, sa, new_L, new_R, i - 1, 
+            max_edits - edit_cost, cigar, no_edits + 1)
+
+    # Insertion
+    cigar += 'I'
+    matches = rec_approx(
+        d, sa, L, R, i - 1, max_edits - 1, cigar, no_edits + 1)
+
+    if new_L != new_R:
+        matches = sa[int(new_L):int(new_R)]
+    
+    return matches
+
+
+def rec_approx(d, sa, L, R, i, edits_left, cigar, no_edits):
+    matches = None
+    lower_limit = d[i]
+    if edits_left < lower_limit:
+        return matches
+    if i < 0: # Means we have a match
+        matches = sa[int(L):int(R)]
+        return matches
+    
+    for a in list(alpha.keys())[1:]:
+        new_L = C[a] + O[int(L)][alpha[a]]
+        new_R = C[a] + O[int(R)][alpha[a]]
+
+        if a == p[i]:
+            edit_cost = 0
+        else:
+            edit_cost = 1
+        if edits_left - edit_cost < 0: continue
+        if new_L >= new_R: continue
+
+        cigar += 'M'
+        rec_approx(d, sa, new_L, new_R, i - 1, edits_left - edit_cost, cigar, no_edits + 1)
+    
+    cigar += 'I'
+    rec_approx(d, sa, L, R, i - 1, edits_left - 1, cigar, no_edits + 1)
+
+    #  Deletion
+    cigar += 'D'
+    for a in list(alpha.keys())[1:]:
+        new_L = C[a] + O[int(L)][alpha[a]]
+        new_R = C[a] + O[int(R)][alpha[a]]
+        if new_L >= new_R: continue
+
+        rec_approx(d, sa, new_L, new_R, i, edits_left - 1, cigar, no_edits + 1)
+
+    matches = sa[int(L):int(R)]
+
+    return matches
+
+bw_approx(O, C, p, d, sa, alpha, 0)
+
+def search_bw(sa, fastq, o_dict, c_dict, ro_dict):
+
+    if len(sa) < 0 or len(fastq) < 0:
+        return "Problems with either fastq file or the SA and LCP"
+
+    flag, mapq, pnext, tlen = 0,0,0,0
+    rnext = "*"
+
+    for x in sa.items():
+        rname = x[0]
+        y = x[1][0] + "$"
+        sa = x[1][1]
+
+        O = o_dict[rname]
+        RO = ro_dict[rname]
+        C = c_dict[rname]
+
+        for p in fastq.items():
+            qname = p[0]
+            substring = p[1][0]
+            cigar = str(len(substring)) + "M"
+            qual = p[1][1]
+
+            alpha = {a:i for i, a in enumerate(sorted(set(y)))}
+            
+            d = d_table(RO)
+            matches = bw_approx(O, C, d, substring, sa, alpha)
+
+            if matches is not None:
+                for match in matches:
+                    pos = int(match) + 1
+                    print(f"{qname}\t{flag}\t{rname}\t{pos}\t{mapq}\t{cigar}\t{rnext}\t{pnext}\t{tlen}\t{substring}\t{qual}", file = sys.stdout)
+    
+    return 
+
+# # Creating first parser
+# parser1 = argparse.ArgumentParser(description='SA computation using SAIS')
+# parser1.add_argument('-p', help='Create SA from fastafile')
+# args1 = parser1.parse_known_args()
+
+# if args1[0].p:
+#     fastafile = parsers.read_fasta_file(args1[0].p)
+#     fastaname = f"{args1[0].p}"
+
+#     # Make suffix array and read file
+#     gen_sa.gen_sa(fastafile, fastaname) # Use Stine's new function here instead
+#     sa = parsers.read_SA(f"{fastaname}.sa")
+#     rsa = parsers.read_SA(f"{fastaname}.rsa") # Make sure that it also creates the sa for the reversed string
+
+#     # Make C, O, and RO table
+#     c_table(sa, fastaname)
+#     o_table(sa, fastaname)
+#     o_table(rsa, fastaname)
+
+
+# else:
+#     # Creating second parser if -p is not given
+#     parser2 = argparse.ArgumentParser(description='Pattern matching using suffix tree')
+#     parser2.add_argument('fastafile', help="Input fasta file")
+#     parser2.add_argument('fastqfile', help="Input fastq file")
+#     args2 = parser2.parse_args()
+#     fastq = parsers.read_fastq_file(args2.fastqfile)
+#     sa = parsers.read_SA(f"{args2.fastafile}.sa")
+    
+#     o_dict = parsers.read_o(f"{args2.fastafile}.o-table")
+#     ro_dict = parsers.read_o(f"{args2.fastafile}.ro-table")
+#     c_dict = parsers.read_c(f"{args2.fastafile}.c-table")
+    
+#     search_bw(sa, fastq, o_dict, c_dict, ro_dict)
